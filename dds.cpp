71f42337f7345a63a69ba1e093bf4dbd6948971d
knarkowicz <k.narkowicz@gmail.com>
2020-05-07 23:17:22 -0400

    Reformated
    * Reformated
    * Updated to VS2017
    * Flipped mouse wheel zoom direction

--- dds.cpp
+++ dds.cpp
@@ -21,113 +21,113 @@ unsigned const DDS_MAGIC = 0x20534444; // "DDS "
 
 struct DDS_PIXELFORMAT
 {
-    uint32_t dwSize;
-    uint32_t dwFlags;
-    uint32_t dwFourCC;
-    uint32_t dwRGBBitCount;
-    uint32_t dwRBitMask;
-    uint32_t dwGBitMask;
-    uint32_t dwBBitMask;
-    uint32_t dwABitMask;
+	uint32_t dwSize;
+	uint32_t dwFlags;
+	uint32_t dwFourCC;
+	uint32_t dwRGBBitCount;
+	uint32_t dwRBitMask;
+	uint32_t dwGBitMask;
+	uint32_t dwBBitMask;
+	uint32_t dwABitMask;
 };
 
 #ifndef MAKEFOURCC
 #   define MAKEFOURCC(ch0, ch1, ch2, ch3) ((uint32_t)(uint8_t)(ch0) | ((uint32_t)(uint8_t)(ch1) << 8) | ((uint32_t)(uint8_t)(ch2) << 16) | ((uint32_t)(uint8_t)(ch3) << 24 ))
 #endif
 
-DDS_PIXELFORMAT const DDSPF_DX10                = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, MAKEFOURCC('D','X','1','0'), 0, 0, 0, 0, 0 };
-DDS_PIXELFORMAT const DDSPF_R16G16B16A16_FLOAT  = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, 113, 0, 0, 0, 0, 0 };
-DDS_PIXELFORMAT const DDSPF_BC6H                = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, 808540228, 0, 0, 0, 0, 0 };
+DDS_PIXELFORMAT const DDSPF_DX10 = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, MAKEFOURCC('D','X','1','0'), 0, 0, 0, 0, 0 };
+DDS_PIXELFORMAT const DDSPF_R16G16B16A16_FLOAT = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, 113, 0, 0, 0, 0, 0 };
+DDS_PIXELFORMAT const DDSPF_BC6H = { sizeof(DDS_PIXELFORMAT), DDS_FOURCC, 808540228, 0, 0, 0, 0, 0 };
 
 struct DDS_HEADER
 {
-    uint32_t dwMagic;
-    uint32_t dwSize;
-    uint32_t dwFlags;
-    uint32_t dwHeight;
-    uint32_t dwWidth;
-    uint32_t dwPitchOrLinearSize;
-    uint32_t dwDepth;
-    uint32_t dwMipMapCount;
-    uint32_t dwReserved1[11];
-    DDS_PIXELFORMAT ddspf;
-    uint32_t dwSurfaceFlags;
-    uint32_t dwCubemapFlags;
-    uint32_t dwReserved2[3];
+	uint32_t dwMagic;
+	uint32_t dwSize;
+	uint32_t dwFlags;
+	uint32_t dwHeight;
+	uint32_t dwWidth;
+	uint32_t dwPitchOrLinearSize;
+	uint32_t dwDepth;
+	uint32_t dwMipMapCount;
+	uint32_t dwReserved1[11];
+	DDS_PIXELFORMAT ddspf;
+	uint32_t dwSurfaceFlags;
+	uint32_t dwCubemapFlags;
+	uint32_t dwReserved2[3];
 };
 
 struct DDS_HEADER_DXT10
 {
-    uint32_t dxgiFormat;
-    uint32_t resourceDimension;
-    uint32_t miscFlag;
-    uint32_t arraySize;
-    uint32_t reserved;
+	uint32_t dxgiFormat;
+	uint32_t resourceDimension;
+	uint32_t miscFlag;
+	uint32_t arraySize;
+	uint32_t reserved;
 };
 
-bool DDS::LoadA16B16G16R16F( char const* filename, SImage& img )
+bool DDS::LoadA16B16G16R16F(char const* filename, SImage& img)
 {
-    img.m_width     = 0;
-    img.m_height    = 0;
-    img.m_data      = nullptr;
-    img.m_dataSize  = 0;
+	img.m_width = 0;
+	img.m_height = 0;
+	img.m_data = nullptr;
+	img.m_dataSize = 0;
 
-    FILE* f = nullptr;
-    fopen_s( &f, filename, "rb" );
-    if ( !f )
-    {
-        return false;
-    }
+	FILE* f = nullptr;
+	fopen_s(&f, filename, "rb");
+	if (!f)
+	{
+		return false;
+	}
 
-    DDS_HEADER hdr;
-    fread( &hdr, sizeof( hdr ), 1, f );
+	DDS_HEADER hdr;
+	fread(&hdr, sizeof(hdr), 1, f);
 
-    if ( hdr.dwMagic == DDS_MAGIC && memcmp( &hdr.ddspf, &DDSPF_R16G16B16A16_FLOAT, sizeof( hdr.ddspf ) ) == 0 )
-    {
-        img.m_dataSize  = hdr.dwWidth * hdr.dwHeight * 8;
-        img.m_data      = new uint8_t[ img.m_dataSize ];
-        img.m_width     = hdr.dwWidth;
-        img.m_height    = hdr.dwHeight;
-        fread( img.m_data, img.m_dataSize, 1, f );
-        fclose( f );
-        return true;
-    }
+	if (hdr.dwMagic == DDS_MAGIC && memcmp(&hdr.ddspf, &DDSPF_R16G16B16A16_FLOAT, sizeof(hdr.ddspf)) == 0)
+	{
+		img.m_dataSize = hdr.dwWidth * hdr.dwHeight * 8;
+		img.m_data = new uint8_t[img.m_dataSize];
+		img.m_width = hdr.dwWidth;
+		img.m_height = hdr.dwHeight;
+		fread(img.m_data, img.m_dataSize, 1, f);
+		fclose(f);
+		return true;
+	}
 
-    fclose( f );
-    return false;
+	fclose(f);
+	return false;
 }
 
-bool DDS::LoadBC6H( char const* filename, SImage& img )
+bool DDS::LoadBC6H(char const* filename, SImage& img)
 {
-    img.m_width     = 0;
-    img.m_height    = 0;
-    img.m_data      = nullptr;
-    img.m_dataSize  = 0;
+	img.m_width = 0;
+	img.m_height = 0;
+	img.m_data = nullptr;
+	img.m_dataSize = 0;
 
-    FILE* f = nullptr;
-    fopen_s( &f, filename, "rb" );
-    if ( !f )
-    {
-        return false;
-    }
+	FILE* f = nullptr;
+	fopen_s(&f, filename, "rb");
+	if (!f)
+	{
+		return false;
+	}
 
-    DDS_HEADER hdr;
-    fread( &hdr, sizeof( hdr ), 1, f );
+	DDS_HEADER hdr;
+	fread(&hdr, sizeof(hdr), 1, f);
 
-    if ( hdr.dwMagic == DDS_MAGIC && memcmp( &hdr.ddspf, &DDSPF_DX10, sizeof( hdr.ddspf ) ) == 0 )
-    {
-        DDS_HEADER_DXT10 hdrDX10;
-        fread( &hdrDX10, sizeof( hdrDX10 ), 1, f );
+	if (hdr.dwMagic == DDS_MAGIC && memcmp(&hdr.ddspf, &DDSPF_DX10, sizeof(hdr.ddspf)) == 0)
+	{
+		DDS_HEADER_DXT10 hdrDX10;
+		fread(&hdrDX10, sizeof(hdrDX10), 1, f);
 
-        img.m_dataSize  = hdr.dwWidth * hdr.dwHeight;
-        img.m_data      = new uint8_t[ img.m_dataSize ];
-        img.m_width     = hdr.dwWidth;
-        img.m_height    = hdr.dwHeight;
-        fread( img.m_data, img.m_dataSize, 1, f );
-        fclose( f );
-        return true;
-    }
+		img.m_dataSize = hdr.dwWidth * hdr.dwHeight;
+		img.m_data = new uint8_t[img.m_dataSize];
+		img.m_width = hdr.dwWidth;
+		img.m_height = hdr.dwHeight;
+		fread(img.m_data, img.m_dataSize, 1, f);
+		fclose(f);
+		return true;
+	}
 
-    fclose( f );
-    return false;
+	fclose(f);
+	return false;
 }
--- dds.h
+++ dds.h
@@ -2,14 +2,14 @@
 
 struct SImage
 {
-    unsigned    m_width;
-    unsigned    m_height;
-    uint8_t*    m_data;
-    unsigned    m_dataSize;
+	unsigned m_width;
+	unsigned m_height;
+	uint8_t* m_data;
+	unsigned m_dataSize;
 };
 
 namespace DDS
 {
-    bool LoadA16B16G16R16F( char const* filename, SImage& img );
-    bool LoadBC6H( char const* filename, SImage& img );
+	bool LoadA16B16G16R16F(char const* filename, SImage& img);
+	bool LoadBC6H(char const* filename, SImage& img);
 }
